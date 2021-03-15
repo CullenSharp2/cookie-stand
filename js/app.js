e28d15d5b7437b786a8c4c1da9959e8dd425d5de
Cullen Sharp <cullen.sharp100@gmail.com>
2021-03-15 14:40:49 -0700

    Add object for Tokyo

--- js/app.js
+++ js/app.js
@@ -1,32 +1,41 @@
 'use strict'
 
-const cookieStand = {
-    name: 'Seattle',
+//assign objects here
+const seattle = {
+    location: 'Seattle',
+    time: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', 'Total'],
+    cookies: ['16 cookies', '20 cookies', '35 cookies', '48 cookies', '56 cookies', '77 cookies', '93 cookies', '144 cookies', '119 cookies', '84 cookies', '61 cookies', '23 cookies', '42 cookies', '57 cookies', '875 cookies'],
+}
+
+const tokyo = {
+    location: 'Tokyo',
     time: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', 'Total'],
     cookies: ['16 cookies', '20 cookies', '35 cookies', '48 cookies', '56 cookies', '77 cookies', '93 cookies', '144 cookies', '119 cookies', '84 cookies', '61 cookies', '23 cookies', '42 cookies', '57 cookies', '875 cookies'],
 }
 
 const bodyElem = document.getElementById('body');
-const newHeader = document.createElement('h2');
-const newUl = document.createElement('ul');
-const newBox = document.createElement('article');
-let newLi;
 
-function createCookieStand(cookieStand){
-bodyElem.appendChild(newBox);
+function createCookieStand(cookieStand) {
+    const newHeader = document.createElement('h2');
+    const newUl = document.createElement('ul');
+    const newBox = document.createElement('article');
+    let newLi;
 
-newBox.appendChild(newHeader);
-newHeader.textContent = cookieStand.name;
+    bodyElem.appendChild(newBox);
 
-newBox.appendChild(newUl);
+    newBox.appendChild(newHeader);
+    newHeader.textContent = cookieStand.location;
 
-for(let i = 0; i < cookieStand.cookies.length; i += 1) {
-    newLi = document.createElement('li');
-    newLi.textContent = `${cookieStand.time[i]}: ${cookieStand.cookies[i]}`
-    newUl.appendChild(newLi);
+    newBox.appendChild(newUl);
 
-    console.log(cookieStand.time[i], cookieStand.cookies[i], );
-} 
+    for (let i = 0; i < cookieStand.cookies.length; i += 1) {
+        newLi = document.createElement('li');
+        newLi.textContent = `${cookieStand.time[i]}: ${cookieStand.cookies[i]}`
+        newUl.appendChild(newLi);
+
+        console.log(cookieStand.time[i], cookieStand.cookies[i], );
+    }
 }
 
-createCookieStand(cookieStand);
+createCookieStand(seattle);
+createCookieStand(tokyo);
