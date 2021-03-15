7c812986f0e0ead91d44a76cbc2659e13f84d7c3
Cullen Sharp <cullen.sharp100@gmail.com>
2021-03-15 14:46:29 -0700

    Add object for Amman

--- js/app.js
+++ js/app.js
@@ -13,6 +13,12 @@ const tokyo = {
     cookies: ['16 cookies', '20 cookies', '35 cookies', '48 cookies', '56 cookies', '77 cookies', '93 cookies', '144 cookies', '119 cookies', '84 cookies', '61 cookies', '23 cookies', '42 cookies', '57 cookies', '875 cookies'],
 }
 
+const amman = {
+    location: 'Amman',
+    time: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', 'Total'],
+    cookies: ['16 cookies', '20 cookies', '35 cookies', '48 cookies', '56 cookies', '77 cookies', '93 cookies', '144 cookies', '119 cookies', '84 cookies', '61 cookies', '23 cookies', '42 cookies', '57 cookies', '875 cookies'],
+}
+
 const bodyElem = document.getElementById('body');
 
 function createCookieStand(cookieStand) {
@@ -39,3 +45,4 @@ function createCookieStand(cookieStand) {
 
 createCookieStand(seattle);
 createCookieStand(tokyo);
+createCookieStand(amman);
